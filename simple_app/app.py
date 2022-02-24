70768df52289693ce71d668a6e0f6b856d9ea2a5
chung i lin <chungilin@207-91-116-150-static.chief.net.tw>
2022-02-24 16:30:13 +0800

    [feature] simple check for API Keys

--- simple_app/app.py
+++ simple_app/app.py
@@ -9,6 +9,9 @@ def create_flask_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     app.config.from_pyfile('config.py', silent=True)
+    # envirenment vars check
+    if not (app.config.get('GOOGLE_API_KEY') and app.config.get('LINE_CHANNEL_ACCESS_TOKEN')):
+        app.logger.warn("Setup Your GOOGLE API Key and Line Channel ACT as envirenment variable correctly")
     # register blueprint
     app.register_blueprint(simple_route)
 
